a0cc94f77dd8cbfc480c9d16a135d111f0b5ecaa
kwonjieon <72382533+kwonjieon@users.noreply.github.com>
2024-07-17 12:50:23 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int answer=0;
+        while(n>=2&&m>=1&&(n+m)-k>=3){
+            answer++;
+            n-=2;
+            m--;
+        }
+        System.out.print(answer);
+
+    }
+}
