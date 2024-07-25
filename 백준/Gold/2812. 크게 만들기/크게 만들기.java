bfc78bb9a50d20039f7d1c2dba48d140a5ff984d
kwonjieon <72382533+kwonjieon@users.noreply.github.com>
2024-07-25 09:06:22 +0900

    [Gold III] Title: 크게 만들기, Time: 424 ms, Memory: 33544 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -7,23 +7,35 @@ public class Main{
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        StringBuffer s = new StringBuffer(br.readLine());
+        String str = br.readLine();
+        Stack<Character> stk = new Stack<>();
+        StringBuffer sb = new StringBuffer();
         int count=0;
         int index=0;
-        while(count<K){
-            if(index>=s.length())
-                break;
-            if(index>0){
-                if(s.charAt(index-1)-'0'<s.charAt(index)-'0'){
-                    s.deleteCharAt(index-1);
-                    index--;
-                    count++;
-                    continue;
-                }
+        while(index<str.length()&&count<K){
+            if(stk.isEmpty()){
+                stk.push(str.charAt(index));
+                index++;
+                continue;
             }
+            if(stk.peek()-'0'<str.charAt(index)-'0'){
+                stk.pop();
+                count++;
+            }
+            else{
+                stk.push(str.charAt(index));
+                index++;
+            }
+        }
+        while(index<str.length()){
+            stk.push(str.charAt(index));
             index++;
         }
-        String str = s.toString();
+        while(!stk.isEmpty()){
+            sb.append(stk.pop());
+        }
+        sb.reverse();
+        str = sb.toString();
         System.out.println(str.substring(0,str.length()-(K-count)));
     }
 }
