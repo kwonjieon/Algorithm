523e07643928e741ebd6c8fd046d2186107c90db
kwonjieon <72382533+kwonjieon@users.noreply.github.com>
2024-07-31 09:30:19 +0900

    [level 2] Title: 점 찍기, Time: 20.34 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        for(long i=0;i<=d;i+=k){
+            long x = (long)Math.sqrt(((long)d*d)-(i*i));
+            answer+=(x/k)+1;
+        }
+        
+        return answer;
+    }
+}
