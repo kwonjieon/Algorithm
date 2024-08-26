f09e4d135e4a1dae8dd955b41f9375d7dac3a6b1
kwonjieon <holemart@naver.com>
2024-08-26 18:43:27 +0900

    [Gold V] Title: 합분해, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[k+1][n+1];
+        for(int i=0;i<=n;i++){
+            dp[1][i]=1;
+        }
+        for(int i=1;i<=k;i++){
+            dp[i][0]=1;
+        }
+        for(int i=2;i<=k;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                dp[i][j]=dp[i][j]%1000000000;
+            }
+        }
+        System.out.println(dp[k][n]);
+
+    }
+}
