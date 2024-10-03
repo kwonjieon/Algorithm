f80dc51844c472091da8ae776916e92bb0f7431b
kwonjieon <holemart@naver.com>
2024-10-03 12:23:02 +0900

    [Gold V] Title: 강의실 배정, Time: 632 ms, Memory: 72608 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,47 +1,47 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
-public class Main {
-    public static void main(String[] args)throws Exception{
+import java.util.*;
+import java.io.*;
+ public class Main{
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         LinkedList<time> list = new LinkedList<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         StringTokenizer st;
         for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            list.add(new time(s,e));
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            list.add(new time(start,end));
+
         }
-        list.sort(new Comparator<time>() {
-            @Override
-            public int compare(time o1, time o2) {
-                if(o1.start==o2.start)
-                    return o1.end- o2.end;
-                return o1.start-o2.start;
+        list.sort(null);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.offer(list.remove(0).end);
+        for(time i:list){
+            if(i.start<pq.peek()){
+                pq.offer(i.end);
             }
-        });
-        int count=0;
-        pq.add(list.remove(0).end);
-        for(time t:list){
-            if(pq.peek()<=t.start){
+            else{
                 pq.poll();
+                pq.offer(i.end);
             }
-            pq.offer(t.end);
+
         }
-        System.out.print(pq.size());
+        System.out.println(pq.size());
+
+
     }
-    static class time{
+    static class time implements Comparable<time>{
         int start;
         int end;
         time(int start,int end){
             this.start=start;
-            this.end=end;
+            this.end = end;
+        }
+        @Override
+        public int compareTo(time o){
+            if(this.start==o.start)
+                return this.end-o.end;
+            return this.start-o.start;
         }
     }
 }
