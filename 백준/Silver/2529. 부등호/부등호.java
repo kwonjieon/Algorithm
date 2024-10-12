a7e0dde63d3dcdec5ecafbfcaab955542fe7214f
kwonjieon <holemart@naver.com>
2024-10-12 22:07:05 +0900

    [Silver I] Title: 부등호, Time: 188 ms, Memory: 22268 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,47 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
-public class Main {
+import java.util.*;
+import java.io.*;
+public class Main{
     static int n;
-    static String str;
-    static StringBuffer sb;
-    static List<String> ans = new ArrayList<>();
-
+    static char[] signs;
+    static LinkedList<String> answer = new LinkedList<>();
     static boolean[] visit;
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        str = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        signs = new char[n];
         visit = new boolean[10];
-        str = str.replaceAll(" ","");
-        sb = new StringBuffer();
+        for(int i=0;i<n;i++){
+            signs[i] = st.nextToken().charAt(0);
+        }
         dfs(0,"");
-        System.out.println(ans.get(ans.size()-1));
-        System.out.println(ans.get(0));
+        System.out.println(answer.get(answer.size()-1));
+        System.out.println(answer.get(0));
+
     }
     static void dfs(int depth,String num){
         if(depth==n+1){
-            ans.add(num);
+            answer.add(num);
             return;
         }
         for(int i=0;i<10;i++){
-            if(!visit[i]&&(depth==0||check(num.charAt(depth-1),(char)(i +'0'),str.charAt(depth-1)))){
+            if(!visit[i]&&(depth==0||check(num.charAt(depth-1),(char)('0'+i),signs[depth-1]))){
                 visit[i]=true;
-                dfs(depth+1,num+Integer.toString(i));
+                dfs(depth+1,num+(char)('0'+i));
                 visit[i]=false;
             }
         }
-
     }
-    static boolean check(char a,char b,char c){
-
-        if(c=='<'){
+    static boolean check(char a,char b,char sign){
+        if(sign=='<'){
             if(a<b) return true;
         }
-        if(c=='>'){
+        else if(sign=='>'){
             if(a>b) return true;
         }
         return false;
