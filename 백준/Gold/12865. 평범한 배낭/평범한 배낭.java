84e02b2543f1d28796a1caddfd05698b04bbac3d
kwonjieon <holemart@naver.com>
2024-10-14 14:14:05 +0900

    [Gold V] Title: 평범한 배낭, Time: 168 ms, Memory: 53796 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,28 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.StringTokenizer;
-
-public class Main {
-    public static void main(String[] args)throws IOException{
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] W = new int[n+1];
-        int[] V = new int[n+1];
-        for (int i = 1; i <= n; i++) {
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] W = new int[N+1];
+        int[] V = new int[N+1];
+
+        for(int i=1;i<=N;i++){
             st = new StringTokenizer(br.readLine());
             W[i] = Integer.parseInt(st.nextToken());
             V[i] = Integer.parseInt(st.nextToken());
-
         }
-        int[][] dp = new int[n+1][k+1];//물건번호, 남은무게
+        int[][] dp = new int[N+1][K+1];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                if(W[i]>j){
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=K;j++){
+                if(j<W[i]){
                     dp[i][j]=dp[i-1][j];
                 }
                 else{
@@ -30,6 +26,7 @@ public class Main {
                 }
             }
         }
-        System.out.print(dp[n][k]);
+        System.out.println(dp[N][K]);
+
     }
 }
