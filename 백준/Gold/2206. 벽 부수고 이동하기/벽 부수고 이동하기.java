6c9593b6683f88f81bea03ed11f760e6036251d0
kwonjieon <holemart@naver.com>
2024-12-23 08:22:21 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 596 ms, Memory: 122772 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,70 +1,64 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-public class Main {
+import java.util.*;
+import java.io.*;
+public class Main{
+    static Queue<point> q = new LinkedList<point>();
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
     static int n;
     static int m;
-    static int[][] map;
     static boolean[][][] visit;
-    static int[] dx = {0,0,-1,1};//상하좌우
-    static int[] dy = {-1,1,0,0};
-    public static void main(String[] args)throws IOException{
+    static int[][] map;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n= Integer.parseInt(st.nextToken());
-        m= Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         map = new int[n][m];
         visit = new boolean[2][n][m];
         for(int i=0;i<n;i++){
             String str = br.readLine();
             for(int j=0;j<m;j++){
-                map[i][j]=str.charAt(j)-'0';
+                map[i][j] = str.charAt(j)-'0';
             }
         }
-        bfs();
+
+        System.out.println(bfs());
+
 
     }
-    static void bfs(){
-        Queue<point> q =new LinkedList<>();
+    static int bfs(){
+        visit[0][0][0] = true;
         q.offer(new point(0,0,0,0));
-        visit[0][0][0]=true;
         while(!q.isEmpty()){
             point p = q.poll();
             if(p.x==m-1&&p.y==n-1){
-                System.out.println(p.count+1);
-                System.exit(0);
+                return p.count+1;
             }
+
             for(int i=0;i<4;i++){
-                int nx = p.x+dx[i];
-                int ny = p.y+dy[i];
-                if(ny>=n||ny<0||nx>=m||nx<0)
-                    continue;
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                if(nx>=m||ny>=n||nx<0||ny<0) continue;
                 if(p.broke==0){
                     if(map[ny][nx]==0&&!visit[p.broke][ny][nx]){
+                        visit[p.broke][ny][nx] = true;
                         q.offer(new point(nx,ny,p.count+1,p.broke));
-                        visit[p.broke][ny][nx]=true;
                     }
                     else if(!visit[p.broke+1][ny][nx]){
+                        visit[p.broke+1][ny][nx] = true;
                         q.offer(new point(nx,ny,p.count+1,p.broke+1));
-                        visit[p.broke+1][ny][nx]=true;
+
                     }
                 }
                 else{
                     if(map[ny][nx]==0&&!visit[p.broke][ny][nx]){
-                        if(!visit[p.broke][ny][nx]){
-                            q.offer(new point(nx,ny,p.count+1,p.broke));
-                            visit[p.broke][ny][nx]=true;
-                        }
+                        visit[p.broke][ny][nx] = true;
+                        q.offer(new point(nx,ny,p.count+1,p.broke));
                     }
-
                 }
             }
         }
-        System.out.println(-1);
+        return -1;
     }
     static class point{
         int x;
@@ -72,11 +66,10 @@ public class Main {
         int count;
         int broke;
         point(int x,int y,int count,int broke){
-            this.x=x;
-            this.y=y;
-            this.count=count;
-            this.broke=broke;
-
+            this.x = x;
+            this.y = y;
+            this.count = count;
+            this.broke = broke;
         }
     }
 }
