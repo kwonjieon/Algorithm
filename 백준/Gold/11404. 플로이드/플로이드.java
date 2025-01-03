7ae728be59be7a563570d1dd54d6d0c9b36abfc7
kwonjieon <holemart@naver.com>
2025-01-03 11:34:03 +0900

    [Gold IV] Title: 플로이드, Time: 528 ms, Memory: 45356 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    static final int INF = 100000000;
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[][] map = new int[N+1][N+1];
+        StringTokenizer st;
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(i==j){
+                    map[i][j]=0;
+                    continue;
+                }
+                map[i][j] = INF;
+            }
+        }
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            map[start][end] = Math.min(map[start][end],value);
+        }
+        //경유지
+        for(int k=1;k<=N;k++){
+            for(int i=1;i<=N;i++){
+                for(int j=1;j<=N;j++){
+                    map[i][j] = Math.min(map[i][j],map[i][k]+map[k][j]);
+                }
+            }
+        }
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(map[i][j]==INF){
+                    map[i][j]= 0;
+                }
+            }
+        }
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                System.out.print(map[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
