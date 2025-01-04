4dd090f15916c4e36a12ade920485a987628c2a8
kwonjieon <holemart@naver.com>
2025-01-04 14:08:13 +0900

    [Gold V] Title: 탑, Time: 2660 ms, Memory: 132924 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+public class Main{
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N+1];
+        int[] ans = new int[N+1];
+        Stack<Integer> stk = new Stack<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1;i<=N;i++){
+            if(stk.isEmpty()){
+                ans[i] = 0;
+                stk.push(i);
+                continue;
+            }
+            if(arr[stk.peek()]<arr[i]){
+                while(!stk.isEmpty()){
+                    if(arr[stk.peek()]<arr[i]){
+                        stk.pop();
+                    }
+                    else{
+                        break;
+                    }
+                }
+                if(stk.isEmpty()){
+                    ans[i]=0;
+                }
+                else{
+                    ans[i] = stk.peek();
+                }
+                stk.push(i);
+            }
+            else{
+                ans[i] = stk.peek();
+                stk.push(i);
+            }
+
+        }
+        for(int i=1;i<=N;i++){
+            System.out.print(ans[i]+" ");
+        }
+    }
+}
