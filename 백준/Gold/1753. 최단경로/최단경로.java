317df82d937a26af75cfd9cb32f94f5cf5663427
kwonjieon <holemart@naver.com>
2025-02-07 18:39:19 +0900

    [Gold IV] Title: 최단경로, Time: 812 ms, Memory: 110188 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,11 +1,11 @@
-import java.util.*;
 import java.io.*;
-public class Main{
+import java.util.*;
 
+public class Main {
     static int V;
     static int E;
     static int start;
-    static LinkedList<Node>[] graph;
+    static List<Node>[] graph;
     static int[] dist;
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,62 +13,57 @@ public class Main{
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
         start = Integer.parseInt(br.readLine());
-        graph = new LinkedList[V+1];
+        graph = new ArrayList[V+1];
+        dist = new int[V+1];
         for(int i=1;i<=V;i++){
-            graph[i] = new LinkedList<Node>();
+            graph[i] = new ArrayList<>();
+            dist[i] = Integer.MAX_VALUE;
         }
         for(int i=0;i<E;i++){
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            graph[s].add(new Node(t,w));
+            graph[s].add(new Node(e,w));
         }
-        dist = new int[V+1];
+        dist[start]=0;
         dijkstra();
         for(int i=1;i<=V;i++){
-            if(dist[i]==1000000){
+            if(dist[i]==Integer.MAX_VALUE){
                 System.out.println("INF");
                 continue;
             }
             System.out.println(dist[i]);
+
         }
 
 
     }
     static void dijkstra(){
-
-        for(int i=1;i<=V;i++){
-            dist[i] = 1000000;
-        }
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2)->o1.weight-o2.weight);
         pq.offer(new Node(start,0));
-        dist[start]=0;
         boolean[] visit = new boolean[V+1];
         while(!pq.isEmpty()){
             Node p = pq.poll();
-            if(visit[p.next])
+
+            if(visit[p.end])
                 continue;
-            visit[p.next]=true;
-            for(Node i : graph[p.next]){
-                if(dist[i.next]>dist[p.next]+i.weight){
-                    dist[i.next] = dist[p.next]+i.weight;
-                    pq.offer(new Node(i.next,dist[i.next]));
+            visit[p.end]=true;
+            for(Node next: graph[p.end]){
+                if(dist[next.end]>dist[p.end]+next.weight){
+                    dist[next.end]=dist[p.end]+next.weight;
+                    pq.offer(new Node(next.end,dist[next.end]));
                 }
             }
         }
-    }
 
-}
-class Node implements Comparable<Node>{
-    int next;
-    int weight;
-    Node(int next,int weight){
-        this.next = next;
-        this.weight = weight;
     }
-    @Override
-    public int compareTo(Node o){
-        return this.weight-o.weight;
+}
+class Node{
+    int end;
+    int weight;
+    Node(int end,int weight){
+        this.end = end;
+        this.weight = weight;
     }
 }
